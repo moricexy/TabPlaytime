4ff5aad8594945404f7d7055467df1c38699bc1f
moricexy <me@mydomain.com>
2023-01-07 16:45:16 +0100

    add files via project

---
+++ src/main/java/de/morice/tabplaytime/TabPlaytime.java
@@ -0,0 +1,98 @@
+package de.morice.tabplaytime;
+
+import de.morice.tabplaytime.config.ConfigManager;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Statistic;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public final class TabPlaytime extends JavaPlugin implements Listener {
+    private ConfigManager configManager;
+
+    @Override
+    public void onEnable() {
+        this.configManager = new ConfigManager(this);
+
+        this.getServer().getPluginManager().registerEvents(this, this);
+        // just in case this is a reload
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            this.updateTab(player);
+        }
+
+        Bukkit.getScheduler().runTaskTimer(this, () -> {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                this.updateTab(player);
+            }
+        }, 300L, 300L);
+    }
+
+    @EventHandler
+    public void onJoin(final PlayerJoinEvent event) {
+        this.updateTab(event.getPlayer());
+    }
+
+    private void updateTab(@NotNull Player player) {
+        final String color = this.configManager.getTabColor();
+        final String type = this.configManager.getWrapType();
+        String mode = this.configManager.getTimeFormat();
+
+        ChatColor c;
+
+        try {
+            c = ChatColor.valueOf(color.toUpperCase());
+        } catch (Exception exception) {
+            c = ChatColor.GREEN;
+            Bukkit.getLogger().warning("Color %s was not found, defaulting *green*");
+        }
+        char first = type.charAt(0);
+        char second = type.charAt(1);
+
+        if (!List.of("default", "min").contains(mode)) {
+            Bukkit.getLogger().warning("Mode %s was not found, defaulting *default*");
+            mode = "default";
+        }
+
+        if (type.length() != 2) {
+            first = '(';
+            second = ')';
+            Bukkit.getLogger().warning("Type was not valid! using default *()*");
+        }
+
+        String time;
+
+        switch (mode) {
+            case "default" -> {
+                final int mins = (player.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20) / 60;
+                final int hours = mins / 60;
+
+                if (hours > 0) {
+                    time = hours + "h";
+                } else {
+                    time = mins + "min";
+                }
+            }
+            case "min" -> {
+                time = (player.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20) / 60 + "min";
+            }
+            default -> throw new IllegalStateException();
+        }
+
+        player.setPlayerListName(this.colorize("&7{a} &8{b}{c}&8{d}"
+                .replace("{a}", player.getName())
+                .replace("{b}", "" + first)
+                .replace("{c}", c + time)
+                .replace("{d}", "" + second)
+        ));
+    }
+
+    private String colorize(@NotNull String colorized) {
+        return ChatColor.translateAlternateColorCodes('&', colorized);
+    }
+}
---
+++ src/main/java/de/morice/tabplaytime/config/ConfigManager.java
@@ -0,0 +1,66 @@
+package de.morice.tabplaytime.config;
+
+import de.morice.tabplaytime.TabPlaytime;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerCommandSendEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ConfigManager {
+    private final TabPlaytime plugin;
+
+    public ConfigManager(@NotNull TabPlaytime plugin) {
+        this.plugin = plugin;
+        this.loadConfig();
+    }
+
+    private void loadConfig() {
+        final FileConfiguration config = this.plugin.getConfig();
+
+        if (!config.isSet("tabColor")) {
+            config.set("tabColor", "GREEN");
+            config.setComments("tabColor", List.of(
+                    "which color will the playtime display have in tab?",
+                    "defaults to GREEN, even when an invalid color is provided")
+            );
+        }
+
+        if (!config.isSet("timeFormat")) {
+            config.set("timeFormat", "default");
+            config.setComments("timeFormat", List.of(
+                    "Represents what format the text is displayed by",
+                    "default: 0min-60min, 5h",
+                    "min: 5h -> 300min"
+            ));
+        }
+
+        if (!config.isSet("wrapType")) {
+            config.set("wrapType", "()");
+            config.setComments("wrapType", List.of(
+                    "What char should be used on end and start of of the format?",
+                    "Must be 2 chars",
+                    "default: ()"
+            ));
+        }
+
+        this.plugin.saveConfig();
+    }
+
+    public String getTabColor() {
+        return this.plugin.getConfig().getString("tabColor", "GREEN");
+    }
+
+    public String getWrapType() {
+        return this.plugin.getConfig().getString("wrapType", "()");
+    }
+
+    public String getTimeFormat() {
+        return this.plugin.getConfig().getString("timeFormat", "default");
+    }
+}
